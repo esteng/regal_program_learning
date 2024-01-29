8f1a3c9d7e5f1c2552af68d2e7dbd183c978c801
Elias Stengel-Eskin <esteng@nlp20.cs.unc.edu>
2024-01-29 18:13:52 -0500

    update script

--- scripts/logo/preprocess.sh
+++ scripts/logo/preprocess.sh
@@ -1,5 +1,6 @@
 #!/bin/bash 
 
+mkdir -p logo_data/images 
 python program_refactoring/domains/logos/generate_programs.py --in_path logo_data/python/train_200.jsonl --out_path logo_data/images/
 
 python program_refactoring/domains/logos/generate_collection.py 
